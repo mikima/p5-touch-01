c462f1f60a0eeada7136cf0b4400cf72ad4a98aa
mikima <mikimauri@gmail.com>
2016-12-06 10:34:22 +0100

    added colors

--- sketch.js
+++ sketch.js
@@ -10,7 +10,15 @@ function draw() {
   var colorList = ['#e86584',
                  '#3c5979',
                  '#018cb7',
-                 '#fae093'];
+                 '#fae093',
+                 '#ffe2e6',
+                 '#6eb66d',
+                 '#ffa960',
+                 '#2b61ce',
+                 '#c496d7',
+                 '#6faaec',
+                 '#fea24d'
+                 ];
   
   for(var i=0; i<touches.length; i++) {
     var touch = touches[i];
